8759358bc6a0cd516cdd8a7aea62cd1fb2eb2b5b
vagrant <vagrant@homestead>
2018-05-29 09:14:30 +0000

    permissions add menu column

---
+++ database/migrations/2018_05_29_170613_add_icon_and_menu_name_and_url_and_parent_id_and_sort_to_permissions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIconAndMenuNameAndUrlAndParentIdAndSortToPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->string('icon')->nullable()->comment('图标');
+            $table->string('menu_name')->default('')->comment('菜单名称');
+            $table->string('url')->nullable()->comment('路径');
+            $table->integer('parent_id')->unsigned()->default(0)->comment('父级路径');
+            $table->integer('sort')->unsigned()->default(0)->comment('排序');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->dropColumn('parent_id');
+            $table->dropColumn('icon');
+            $table->dropColumn('menu_name');
+            $table->dropColumn('url');
+            $table->dropColumn('sort');
+        });
+    }
+}
